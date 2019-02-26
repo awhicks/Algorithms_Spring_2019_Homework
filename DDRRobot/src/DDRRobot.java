50f5d10488f72cdfab8834cf3081988805c71fd5
awh4kc <awh4kc@virginia.edu>
2019-02-26 12:40:07 -0500

    Skeleton code for DDRRobot

---
+++ DDRRobot/src/DDRRobot.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class DDRRobot {
+
+    public Digraph G = null;
+    private int Demension;
+
+    /**
+     * Initalizes the DDRobot Board
+     * @param squares
+     */
+    public DDRRobot(int squares){
+        // TODO: implement this method
+    }
+
+    /**
+     * key:
+     * → 1
+     * ↑ 2
+     * ← 3
+     * ↓ 4
+     * @param square  based o the square number
+     * @param arrow based on tile arrow number
+     */
+
+    public void addTile(int square, int arrow){
+        // TODO: implement this method
+    }
+
+    /**
+     * Retuns the list of Tiles
+     */
+    public ArrayList<Integer> getPlayOptions(){
+        // TODO: implement this method
+        return getPlayOptions();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ DDRRobot/src/Digraph.java
@@ -0,0 +1,127 @@
+/**
+ * Orginial Authors: Robert Sedwick and Kevin wayne
+ * MOdified by Daniel Graham
+ */
+
+
+import java.util.ArrayList;
+
+
+public class Digraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private final int V;           // number of vertices in this digraph
+    private int E;                 // number of edges in this digraph
+    private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+
+
+    public Digraph(int V)
+    {
+        this.V = V;
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new ArrayList<Integer>();
+    }
+
+
+    /**
+     * Returns the number of vertices in this digraph.
+     *
+     * @return the number of vertices in this digraph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this digraph.
+     *
+     * @return the number of edges in this digraph
+     */
+    public int E() {
+        return E;
+    }
+
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the directed edge v→w to this digraph.
+     *
+     * @param  v the tail vertex
+     * @param  w the head vertex
+     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(w);
+        E++;
+    }
+
+    /**
+     * Returns the vertices adjacent from vertex {@code v} in this digraph.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the number of directed edges incident from vertex {@code v}.
+     * This is known as the <em>outdegree</em> of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the outdegree of vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+
+    /**
+     * Returns the reverse of the digraph.
+     *
+     * @return the reverse of the digraph
+     */
+    public Digraph reverse() {
+        Digraph reverse = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                reverse.addEdge(w, v);
+            }
+        }
+        return reverse;
+    }
+
+    /**
+     * Returns a string representation of the graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(String.format("%d: ", v));
+            for (int w : adj[v]) {
+                s.append(String.format("%d ", w));
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+
+}
