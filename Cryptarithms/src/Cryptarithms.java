18f38799730676c855570f78346720734905c5b2
awh4kc <awh4kc@virginia.edu>
2019-02-28 17:01:29 -0500

    Skeleton Code for Cryptarithms

---
+++ Cryptarithms/src/Cryptarithms.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Cryptarithms {
+
+
+    /**
+     * @param equation a list of strings that make up the equation, last element is the sum, all other elements are addends
+     * @return a map from characters to integers representing your final solution
+     */
+    public static Map<Character, Integer> solve(List<String> equation) {
+        // TODO: implement this method
+        return null;
+    }
+
+
+    /**
+     * Add for compilation and incremental testing purposes
+     * @param args
+     */
+    public static void main(String args[]) {
+        System.out.println("Welcome to Cryptarithms!");
+    }
+}
---
+++ Cryptarithms/src/CryptarithmsTest.java
@@ -0,0 +1,326 @@
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.Assert.*;
+
+public class CryptarithmsTest {
+
+    /**
+     * This takes in the challenge and the answer from the student solution and checks to see if the solution is valid
+     * NOTE: only works if the answer isn't NULL
+     * @param cryptChall
+     * @param candAns
+     * @return
+     */
+    public static boolean isSolValid(List<String> cryptChall, Map<Character, Integer> candAns) {
+        int answer = 0;
+        int sumNum = 0;
+        String sum = cryptChall.remove(cryptChall.size() - 1);
+        int lengthOfSum = sum.length();
+
+        // Calculates the sum
+        for (int i = 0; i < lengthOfSum; i++) {
+            sumNum += candAns.get(sum.charAt(i)) * (Math.pow(10, lengthOfSum - 1 - i));
+        }
+
+        // loops through the list of strings
+        for (int i = 0; i < cryptChall.size(); i++) {
+            String current = cryptChall.get(i);
+            int lenCurrent = current.length();
+            // loops through current string and computes sum on that smaller number
+            for (int j = 0; j < lenCurrent; j++) {
+                answer += candAns.get(current.charAt(j)) * Math.pow(10, lenCurrent - 1 - j);
+            }
+        }
+        boolean ret = answer == sumNum;
+        if (!ret) {
+            System.out.println(candAns + " incorrect for ");
+            System.out.println(cryptChall + " = " +sum);
+        }
+        return ret;
+    }
+
+    @Test(timeout=200)
+    public void solveSendMoreMoney() {
+        List<String> input = new ArrayList<>();
+        input.add("SEND");
+        input.add("MORE");
+        input.add("MONEY");
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('E', 5);
+        expected.put('S', 9);
+        expected.put('N', 6);
+        expected.put('D', 7);
+        expected.put('M', 1);
+        expected.put('O', 0);
+        expected.put('R', 8);
+        expected.put('Y', 2);
+
+        assertTrue(isSolValid(input, Cryptarithms.solve(input)));
+
+    }
+
+    @Test(timeout=200)
+    public void planets1() {
+        List<String> input = new ArrayList<>();
+        input.add("MARS");
+        input.add("SATURN");
+        input.add("URANUS");
+        input.add("MERCURY");
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('A', 9);
+        expected.put('C', 6);
+        expected.put('E', 2);
+        expected.put('M', 1);
+        expected.put('N', 0);
+        expected.put('R', 3);
+        expected.put('S', 4);
+        expected.put('T', 5);
+        expected.put('U', 7);
+        expected.put('Y', 8);
+
+        assertTrue(isSolValid(input, Cryptarithms.solve(input)));
+
+    }
+
+    @Test(timeout=200)
+    public void planets2() {
+        List<String> input = new ArrayList<>();
+        input.add("SATURN");
+        input.add("URANUS");
+        input.add("JUPITER");
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('A', 4);
+        expected.put('E', 2);
+        expected.put('I', 8);
+        expected.put('J', 1);
+        expected.put('N', 7);
+        expected.put('P', 0);
+        expected.put('R', 6);
+        expected.put('S', 9);
+        expected.put('T', 3);
+        expected.put('U', 5);
+
+        assertTrue(isSolValid(input, Cryptarithms.solve(input)));
+
+    }
+
+    @Test(timeout=200)
+    public void planets3() {
+        List<String> input = new ArrayList<>();
+        input.add("VENUS");
+        input.add("EARTH");
+        input.add("URANUS");
+        input.add("SATURN");
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('A', 2);
+        expected.put('E', 7);
+        expected.put('H', 4);
+        expected.put('N', 0);
+        expected.put('R', 5);
+        expected.put('S', 8);
+        expected.put('T', 1);
+        expected.put('U', 6);
+        expected.put('V', 9);
+
+        assertTrue(isSolValid(input, Cryptarithms.solve(input)));
+
+    }
+
+    @Test(timeout=200)
+    public void planets4() {
+        List<String> input = new ArrayList<>();
+        input.add("EARTH");
+        input.add("URANUS");
+        input.add("SATURN");
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('A', 4);
+        expected.put('E', 9);
+        expected.put('H', 3);
+        expected.put('N', 0);
+        expected.put('R', 5);
+        expected.put('S', 7);
+        expected.put('T', 8);
+        expected.put('U', 6);
+
+        assertTrue(isSolValid(input, Cryptarithms.solve(input)));
+
+    }
+
+    @Test(timeout=2000)
+    public void planets5Null() {
+        List<String> input = new ArrayList<>();
+        input.add("VENUS");
+        input.add("MARS");
+        input.add("SATURN");
+        input.add("URANUS");
+
+        assertEquals(null, Cryptarithms.solve(input));
+
+    }
+
+    public static Set<Character> getAlphabet() {
+        Set<Character> out = new HashSet<>();
+        String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < alph.length(); i++) {
+            out.add(alph.charAt(i));
+        }
+        return out;
+    }
+
+    public static <T> T setPop(Set<T> s) {
+        Object[] a = s.toArray();
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        T t = (T)a[rnd.nextInt(a.length)];
+        s.remove(t);
+        return t;
+    }
+
+    /**
+     * Outputted problem is guaranteed to have a solution
+     *
+     * @param numAddends
+     * @param maxAddend
+     * @return
+     */
+    public static List<String> getRandProblem(int numAddends, long maxAddend) {
+        //System.out.println(maxAddend);
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        long[] addends = new long[numAddends];
+        for (int i = 0; i < numAddends; i++)
+            addends[i] = rnd.nextLong(maxAddend - 1) + 1;
+        long sum = 0;
+        for (long i : addends)
+            sum += i;
+
+        List<String> numberStrings = new ArrayList<>();
+        for (long a : addends)
+            numberStrings.add("" + a);
+        numberStrings.add("" + sum);
+        //System.out.println(numberStrings);
+
+        return encodeNumberStrings(numberStrings);
+    }
+
+    public static List<String> encodeNumberStrings(List<String> numberStrings) {
+        Set<Integer> uniqueDigits = new HashSet<>();
+        for (String s : numberStrings)
+            for (int i = 0; i < s.length(); i++)
+                uniqueDigits.add(s.charAt(i) - '0');
+        //System.out.println(uniqueDigits);
+
+
+        Set<Character> alphabet = getAlphabet();
+        Map<Integer, Character> m = new HashMap<>();
+        while (!uniqueDigits.isEmpty())
+            m.put(setPop(uniqueDigits), setPop(alphabet));
+
+        List<String> problem = new ArrayList<>();
+        for (String s : numberStrings) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++)
+                sb.append(m.get(s.charAt(i) - '0'));
+            problem.add(sb.toString());
+        }
+        return problem;
+    }
+
+
+
+    @Test(timeout=20000)
+    public void randomTest() {
+        ThreadLocalRandom randomness = ThreadLocalRandom.current();
+        int width = 10;
+        int height = 10;
+
+        for (int i = 1; i < width; i++) {
+            for (int h = 2; h < height; h++) {
+                List<String> problem = getRandProblem(h, Math.round(Math.pow(10, i)));
+
+                assertTrue(
+                        isSolValid(
+                                problem,
+                                Cryptarithms.solve(
+                                        Collections.unmodifiableList(
+                                                problem
+                                        )
+                                )
+                        )
+                );
+            }
+        }
+    }
+
+
+    public String getNums(int duplicates, boolean leading) {
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int i = 0; i < 10; i++)
+            counts.put(i, duplicates);
+
+        StringBuilder out = new StringBuilder();
+        while (!counts.isEmpty()) {
+            Object[] keys = counts.keySet().toArray();
+            int n = (int)keys[rnd.nextInt(keys.length)];
+            counts.replace(n, counts.get(n) -1);
+            if (counts.get(n) <= 0)
+                counts.remove(n);
+            out.append(n);
+        }
+        if (leading) {
+            for (int i = 0; i < out.length(); i++)
+                if (out.charAt(i) == '0') {
+                    out.replace(i, i + 1, "" + (rnd.nextInt(9) + 1));
+                }
+        }
+        return out.toString();
+    }
+
+
+    @Test(timeout = 20000)
+    public void runtimeTest() {
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        int d = 4;
+
+        List<String> nums = new ArrayList<>();
+        for (int i = 0; i < 10*d; i++) {
+            nums.add(getNums(d, i==0));
+        }
+
+        List<String> numberStrings = new ArrayList<>();
+        for (int i = 0; i < 10*d; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int c = 0; c < nums.size(); c++)
+                sb.append(nums.get(c).charAt(i));
+            numberStrings.add(sb.toString());
+        }
+        numberStrings.remove(rnd.nextInt(10*d));
+
+        BigInteger sum = new BigInteger("0");
+        for (String s : numberStrings) {
+            sum = sum.add(new BigInteger(s));
+            //System.out.println(s);
+        }
+        numberStrings.add(sum.toString());
+        //System.out.println(sum);
+        List<String> problem = encodeNumberStrings(numberStrings);
+        //for (String s : problem)
+        //    System.out.println(s);
+        assertTrue(
+                isSolValid(
+                        problem,
+                        Cryptarithms.solve(
+                                Collections.unmodifiableList(
+                                        problem
+                                )
+                        )
+                )
+        );
+    }
+
+
+}
